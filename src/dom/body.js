8a2ecdb8cde86be7f3f678eeb5367b24438e72ce
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-05-15 13:49:53 +0300

    adds background image

---
+++ src/dom/body.js
@@ -0,0 +1,8 @@
+const body = () => {
+  const fullBody = document.createElement('div');
+  fullBody.classList.add('vh-100', 'fullBody');
+
+  return fullBody;
+};
+
+export default body;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import 'bootstrap';
 import './scss/index.scss';
+import body from './dom/body';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
-  element.classList.add('bg-info', 'body');
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+document.body.appendChild(body());
