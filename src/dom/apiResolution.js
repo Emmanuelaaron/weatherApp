193775918749b7b0cedbff44e7e619efaf410c82
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-06-03 22:06:48 +0300

    Adds provision for convertig the celsius to fahrenheit and viceversa

--- src/dom/apiResolution.js
+++ src/dom/apiResolution.js
@@ -9,44 +9,31 @@ const apiRes = (name, response) => {
   const myTable = document.getElementById('myTable');
   myTable.innerHTML = '';
 
-  const display = document.createElement('p');
-  display.innerHTML = 'display in °F';
-  display.id = 'temp';
+  const formHeader = document.getElementById('headerForm')
 
-  const toggleButton = document.createElement('div')
-  toggleButton.classList.add('custom-control', 'custom-switch')
+  const dButton = document.createElement('button')
+  dButton.innerText = 'Display in °F'
+  dButton.id = 'tempButton'
+  dButton.classList.add('btn', 'btn-primary', 'ml-2')
 
-  const toggleInput = document.createElement('input')
-  toggleInput.classList.add('custom-control-input')
-  toggleInput.type = 'checkbox'
-  // toggleInput.checked = true
-  // toggleInput.setAttribute("data-toggle", "toggle")
-
-  toggleInput.id = 'customSwitch1'
-
-  const toggleLabel = document.createElement('label')
-  toggleLabel.classList.add('custom-control-label')
-  toggleLabel.innerHTML = 'weater'
-  toggleLabel.setAttribute("for", "customSwitch2")
-
-  toggleButton.appendChild(toggleInput)
-  toggleButton.appendChild(toggleLabel)
-
-  display.addEventListener('click', (e) => {
+  dButton.addEventListener('click', (e) => {
     e.preventDefault();
     const displayLink = document.getElementById('temp');
     const tempDegree = document.getElementById('tempDegrees');
     const tempMetric = document.getElementById('tempMetric');
-    if (displayLink.innerHTML === 'display in °F') {
-      displayLink.innerHTML = 'display in °C';
+    if (dButton.innerText === 'display in °F') {
+      dButton.innerText = 'display in °C';
       tempDegree.innerHTML = 'Temp(°F)';
       tempMetric.innerHTML = celsiusToFahrenheit(response.main.temp);
     } else {
-      displayLink.innerHTML = 'display in °F';
+      dButton.innerText = 'display in °F';
       tempDegree.innerHTML = 'Temp(°C)';
       tempMetric.innerHTML = response.main.temp;
     }
   });
+
+  formHeader.appendChild(dButton)
+
   myTable.appendChild(
     myResultsTable(
       response.main.humidity,
@@ -54,8 +41,6 @@ const apiRes = (name, response) => {
       response.main.temp,
     ),
   );
-  myTable.appendChild(display);
-  myTable.appendChild(toggleButton)
 };
 
 export default apiRes;
